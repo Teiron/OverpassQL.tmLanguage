4429a1e84a93407cd8355178e02f96d6b1000ca3
Teiron <mmok444@gmail.com>
2017-04-04 22:27:03 +0200

    fix bug in python file

--- yaml2json.py
+++ yaml2json.py
@@ -2,9 +2,8 @@ import simplejson as json
 import yaml
 
 
-with open('syntaxes\overpassQL.yaml', 'r') as origin:
+with open('overpass\syntaxes\overpassQL.yaml', 'r') as origin:
     data = yaml.load(origin)
 
-with open("syntaxes\overpassQL.json", "w") as target:
+with open("overpass\syntaxes\overpassQL.json", "w") as target:
     target.write(json.dumps(data, indent=3 * ' '))
-
